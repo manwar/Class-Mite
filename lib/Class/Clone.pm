d93d47c033d4fdabe8df133d3de95bb3ca687330
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2025-10-13 10:52:55 +0100

    - Added Class::Clone

---
+++ lib/Class/Clone.pm
@@ -0,0 +1,50 @@
+package Class::Clone;
+
+use strict;
+use warnings;
+
+sub import {
+    my ($class, @args) = @_;
+    my $caller = caller(0);
+
+    no strict 'refs';
+
+    # Install clone method if not already defined
+    if (!defined &{"${caller}::clone"}) {
+        *{"${caller}::clone"} = sub {
+            my $self = shift;
+            my %attrs = (%$self, @_);  # Shallow copy + allow overrides
+            return ref($self)->new(%attrs);
+        };
+    }
+    # If clone already exists, we do nothing (no conflicts)
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Class::Clone - Add clone method to Class-based classes
+
+=head1 SYNOPSIS
+
+    package My::Class;
+    use Class;
+    use Class::Clone;
+
+    has name => (required => 1);
+    has age  => (default => 0);
+
+    package main;
+    my $original = My::Class->new(name => 'John', age => 30);
+    my $clone = $original->clone;
+    my $modified = $original->clone(name => 'Jane', age => 25);
+
+=head1 DESCRIPTION
+
+Provides a C<clone> method that creates a shallow copy of objects.
+Accepts optional attribute overrides.
+
+=cut
